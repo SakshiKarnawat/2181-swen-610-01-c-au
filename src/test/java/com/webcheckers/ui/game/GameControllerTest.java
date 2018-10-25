9c07ffe9b29b94aba59aecb2dea301849fe3923c
Sakshi Karnawat <sakshik999@gmail.com>
2018-10-25 12:02:52 -0400

    test

---
+++ src/test/java/com/webcheckers/ui/game/GameControllerTest.java
@@ -0,0 +1,15 @@
+package com.webcheckers.ui.game;
+
+import com.webcheckers.appl.GameCenter;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameControllerTest {
+
+    @Test
+    public void ModelAndView() {
+
+        assertTrue(ModelAndView());
+    }
+}
