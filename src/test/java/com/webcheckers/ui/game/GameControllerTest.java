82b3d5b2670469b8fa045d3ab4fb4c97d3ce6ef2
Sakshi Karnawat <sakshik999@gmail.com>
2018-10-24 22:59:31 -0400

    test

---
+++ src/test/java/com/webcheckers/ui/game/GameControllerTest.java
@@ -0,0 +1,15 @@
+package com.webcheckers.ui.game;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+/*
+public class GameControllerTest {
+    @Test
+    public void gameControlsTesting throws Exception {
+
+
+    }
+
+}
+    */
