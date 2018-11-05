532ee910d5fcb19bd26c369d0c7429dd98d0f590
Alex <ahreinhardt@gmail.com>
2018-11-05 14:35:46 -0500

    fixed col
    I missed to col that should be cell

--- src/main/java/com/webcheckers/model/Piece.java
+++ src/main/java/com/webcheckers/model/Piece.java
@@ -52,8 +52,8 @@ public class Piece {
         return cell;
     }
 
-    public void setCell(int col) {
-        this.cell = col;
+    public void setCell(int cell) {
+        this.cell = cell;
     }
 
     public String getColor() {
